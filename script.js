ab01f1b6ec2eae74e75e3337f631ff019e524c9d
rfloarea <account@rfloarea.net>
2024-09-11 06:54:14 -0400

    variables and elements for month and year

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("connected");
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
+
+const currentDate = document.querySelector('.calendar-current-date');
+const day = document.querySelector('.calendar-dates');
