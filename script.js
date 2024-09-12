e56c593e2f96d34101d6d27305d117e0cb962277
rfloarea <account@rfloarea.net>
2024-09-12 14:03:04 -0400

    clean up and comment

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+// Initialize our dates
 let date = new Date();
-console.log("Current date:", date)
 let year = date.getFullYear();
 let month = date.getMonth();
 
+// select our elements
 const currentDate = document.querySelector('.calendar-current-date');
 const day = document.querySelector('.calendar-dates');
 
-// A list of months so we can use our numerical month from the Date object
-// as the index in an array. See, currentDate.textContent
+// Create list of months so we can use our numerical months from the Date object
+// as the index in an array. See, currentDate.textContent for use case
 const months = [
   "January",
   "February",
@@ -28,37 +29,39 @@ function genCal() {
   // Get the first day of the month
   const dayFirst = new Date(year, month, 1).getDay();
   // console.log("First day of the month:", dayFirst);
+
   // Get the last date of the month
   const dateLast = new Date(year, month + 1, 0).getDate();
   // console.log("Last date of the month:", dateLast);
+
   // Get the day of `dateLast`
   const dayLast = new Date(year, month, dateLast).getDay();
   // console.log("The day of the last date of the month (0-6 == sun-sat):", dayLast);
+
   // Get the last day date of previous month
   const monthLastDayDate = new Date(year, month, 0).getDate();
   // console.log("Last day date of the previous month:", monthLastDayDate);
 
-  // Init variable to hold the generated HTML
+  // Init our variable to hold the generated HTML
   let calHTML = '';
   // Generate elements for previous month's dates
   for (let i = dayFirst; i > 0; i--) {
-    // console.log(dayFirst);
+    // and apply "prev-month" class
     calHTML += `<li class="prev-month">${monthLastDayDate - i + 1}</li>`;
   };
 
   // Generate elements for current month's dates
+  // and apply correct classes.
   for (let i = 1; i <= dateLast; i++) {
-    // console.log(i);
-    // Apply class to the element that is the current date
-    // If today is the the right day of the week
+    // If i is the same as the date for this month
     let isToday = i === date.getDate()
     // and if we are in the right month
     && month === new Date().getMonth()
     // and if it is the right year
     && year === new Date().getFullYear()
-    // then apply a class of "active"
+    // then apply a "today" class
     ? "today"
-    // if not, leave class blank
+    // if not, apply a "this-month" class
     : "this-month"
 
     calHTML += `<li class="${isToday}">${i}</li>`;
@@ -66,30 +69,28 @@ function genCal() {
 
   // Generate elements for next month's dates
   for (let i = dayLast; i < 6; i++) {
-    // console.log(i);
+    // and apply a "next-month" class
     calHTML += `<li class="next-month">${i - dayLast + 1}</li>`;
   };
 
-  // Set calendar header to correct month and year
+  // Update calendar header to correct month and year
   currentDate.textContent = `${months[month]} ${year}`
 
-  // Display calendar
+  // Feed our day element the HTML we generated
   day.innerHTML = calHTML;
 };
 
+// call that baby!
 genCal();
-
-
-
 
 /* 
   This section handles "moving" to previous or next month.
   But it doesn't really "move". It triggers a regeneration of the currently displayed month.
   This gives the illusion of moving, because *we* imagine it that way.
-  (We can also do some fancy transition magic to play it up more.)
+  (We can also do some fancy transition magic to play it up more if you'd like.)
   
   So all we're doing here is saying, "ah, the left arrow icon was clicked.
-  Let's generate new HTML to replace the current month with the month before it".
+  Let's generate new HTML to replace the current month with the dates of the month before it".
   The same but reversed for the right arrow.
 */
 
@@ -112,48 +113,60 @@ If it is either of those, we have to create new dates for that new year.
 If it's not (if it's within bounds), then we continue as usual.
 */ 
 
-// Event handlers
+/** 
+ * Two event handlers!
+ * There are ways to group these two functions 
+ * by calling forEach on the icons. I'm not doing that because I like how
+ * redundant this is :P
+ */
+
 function handleGenPrevMonth() {
-  // Decrement the month variable
+  // Decrement the month
   month = month - 1;
-  console.log(month)
 
   if (month < 0 || month > 11) {
     // This sets `date` as the current date of the month in the new year 
     date = new Date(year, month, new Date().getDate());
-    console.log(date);
-    // year number
+    // console.log(date);
+    // Get our year
     year = date.getFullYear();
-    console.log(year);
-    // month number
+    // console.log(year);
+    // Get our month
     month = date.getMonth();
-    console.log(month);
-  } else {
-    // or else basically do nothing?
-    date = new Date();
-  };
-  // Generate a new calendar
-  genCal();
-};
-function handleGenNextMonth() {
-  // Increment the month
-  month = month + 1;
-  console.log(month)
-
-  if (month < 0 || month > 11) {
-    // This sets `date` as the current date of the month in the new year 
-    date = new Date(year, month, new Date().getDate());
-    console.log(date);
-    // year number
-    year = date.getFullYear();
-    console.log(year);
-    // month number
-    month = date.getMonth();
-    console.log(month);
+    // console.log(month);
   } else {
     // or else basically do nothing
     date = new Date();
   };
-  // Generate a new calendar
+  // Generate a new calendar with these new variables
   genCal();
 };
+
+function handleGenNextMonth() {
+  // Increment the month
+  month = month + 1;
+  // console.log(month)
+
+  if (month < 0 || month > 11) {
+    // This sets `date` as the current date of the month in the new year 
+    date = new Date(year, month, new Date().getDate());
+    // console.log(date);
+    // Get our year
+    year = date.getFullYear();
+    // console.log(year);
+    // Get our month
+    month = date.getMonth();
+    // console.log(month);
+  } else {
+    // or else basically do nothing
+    date = new Date();
+  };
+  // Generate a new calendar with these new variables
+  genCal();
+};
+
+/**
+ * As you can see, the greater portion of these handlers are the same.
+ * We could set up a condition that determines if we should increment or decrement month.
+ * The rest should take care of itself.
+ */
